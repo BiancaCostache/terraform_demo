0110d30b4b874084c52d25de8a7353e9dfd2ea2e
Bianca Costache <bianca.costache95@yahoo.com>
2025-01-19 11:54:16 +0000

    initial commit

---
+++ new_bucket.tf
@@ -0,0 +1,25 @@
+# Specify the provider and access detailss
+
+provider "aws" {
+  
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "udabucket" {
+  bucket = "cicd-terraform-demo-bucket20213"
+
+  tags = {
+    Name        = "CICD test bucket"
+    Environment = "Dev"
+  }
+}
+
+# Create an EC2 instance
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-0df8c184d5f6ae949"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "CICD test instance"
+  }
+}
